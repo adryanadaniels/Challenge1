be1e0d1c7f3bf996ece50eaf9600628ba33bfb0c
Aria <adryanadaniels@gmail.com>
2026-09-21 13:10:49 -0500

    LinkList header structure

---
+++ Linklist.h
@@ -0,0 +1,42 @@
+//LinkList.h (header file for the LinkList class and node struct)
+#ifndef LINKLIST_H
+#define LINKLIST_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node
+{
+    std::string data;
+    Node* next;
+};
+
+class LinkList
+{
+private:
+    Node* head;
+    Node* tail;
+public:
+// default constructor and destructor and copy constructor
+    LinkList();
+    ~LinkList();
+    LinkList(const LinkList& old);
+
+// methods for the linked list
+    //insert
+    void append(const string& data); //insert data after the tail
+    void prepend(const string& data); //insert data before the head
+
+    //search
+    bool search (const string& data);
+
+    //removal
+    bool remove(const string& data);
+    bool removeBack(const string& data);
+
+    //display 
+    void display(ostream& out);
+    void diplayBack(ostream& out);
+
+};
+#endif
