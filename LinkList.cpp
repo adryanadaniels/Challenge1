f6d3ea56889026d3d02ed3f1ce1ecc0f09125b1a
Aria <adryanadaniels@gmail.com>
2026-09-25 12:03:37 -0500

    changes to display

--- LinkList.cpp
+++ LinkList.cpp
@@ -130,9 +130,10 @@ LinkList::LinkList(const LinkList& old)
         Node *tmp = head; // set the current tmp node to the head
         while (tmp != nullptr) // loop while the node is still in the list
         {
-            out << tmp->data << endl; // output the data stored in the current node
+            out << tmp->data << " "; // output the data stored in the current node
             tmp = tmp->next; // set tmp to the next node in the chain 
         }
+        cout << endl;
     }
     void LinkList::displayBack(ostream& out)
     {
